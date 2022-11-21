cce2564afd373553b04872d9d93fc3448d981166
Elvis Guanoluisa <elviserock@gmail.com>
2022-11-21 12:00:47 -0500

    creando constructores

--- src/series.java
+++ src/series.java
@@ -1,2 +1,7 @@
 public class series {
+    int numeroTemporadas;
+    String titulo;
+    boolean entregado;
+    String genero;
+    String creador;
 }
--- src/videojuegos.java
+++ src/videojuegos.java
@@ -1,2 +1,14 @@
 public class videojuegos {
+    String titulo;
+    int horasEstimadas;
+    boolean entregado;
+    String genero;
+    String compania;
+
+    public videojuegos(){
+        titulo = " ";
+        horasEstimadas = ;
+        entregado = " ";
+
+    }
 }
